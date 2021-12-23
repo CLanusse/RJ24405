0ddffcabaeceefe4f753e01d51b0e3b87bc35937
Conrado Lanusse <conrado.lanusse@gmail.com>
2021-12-23 20:47:14 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MiModulo from './MiModulo'
 
 function App() {
+
   return (
     <div className="App">
+      <MiModulo />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo Coders!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>REACT JS CODERHOUSE</h3>
       </header>
     </div>
   );
---
+++ src/MiModulo.js
@@ -0,0 +1,14 @@
+
+
+const MiModulo = () => {
+
+    return (
+            <section>
+                <h2>Hola mundo!</h2>
+                <hr/>
+                <p>Algún contenido genérico</p>
+            </section>
+        )
+}
+
+export default MiModulo
